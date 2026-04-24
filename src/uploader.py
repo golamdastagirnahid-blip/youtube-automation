1e7ba9d382b82538495a1897aba899859b2946c1
golamdastagirnahid-blip <golamdastagirnahid@gmail.com>
2026-04-24 16:09:44 +0800

    Update uploader.py

--- src/uploader.py
+++ src/uploader.py
@@ -7,7 +7,7 @@ from src.config              import (
 )
 from src.database            import Database
 from src.title_generator     import TitleGenerator
-from src.thumbnail_editor    import ThumbnailEditor
+from src.thumbnail_editor import ThumbnailGenerator as ThumbnailEditor
 
 
 class VideoUploader:
